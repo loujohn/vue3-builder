0975fe38a319464c4cf8b4756afb5edeb64cf76b
lou <825681860@qq.com>
2021-06-28 17:57:27 +0800

    build(vite 配置修改): build(添加vite配置): 🔧 build: 添加vite基础配置

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,11 +1,13 @@
 import type { Plugin } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins: (Plugin | Plugin[])[] = [
     // have to
-    vue()
+    vue(),
+    vueJsx()
   ];
   return vitePlugins;
 }
