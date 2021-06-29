1a2d96825193c554ef3d5b6fb462a2e732112928
lou <825681860@qq.com>
2021-06-29 10:35:13 +0800

    chore(deps): 🐳 chore: 添加svg-icons

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,6 +2,8 @@ import type { Plugin } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { configStyleImportPlugin } from './styleImport';
+import { configSvgIconsPlugin } from './svgSprite';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins: (Plugin | Plugin[])[] = [
@@ -9,5 +11,9 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
     vue(),
     vueJsx()
   ];
+  // vite-plugin-svg-icons
+  vitePlugins.push(configSvgIconsPlugin(isBuild));
+  // vite-plugin-style-import
+  vitePlugins.push(configStyleImportPlugin(isBuild));
   return vitePlugins;
 }
