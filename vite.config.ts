3b3630653a62ac36f64380db38738d73a682b1cb
lou <825681860@qq.com>
2021-06-29 15:59:50 +0800

    chore(deps): 🐳 chore: 修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { ConfigEnv, loadEnv, UserConfig } from 'vite';
 import { createVitePlugins } from './build/vite/plugin';
 import { wrapperEnv } from './build/utils';
+import { createProxy } from './build/vite/proxy';
+import { OUTPUT_DIR } from './build/constant';
+
+import pkg from './package.json';
+import moment from 'moment';
+
+const { dependencies, devDependencies, name, version } = pkg;
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: moment().format('YYYY-MM-DD HH:mm:ss')
+};
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => {
   const root = process.cwd();
@@ -9,15 +20,33 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
   // The boolean type read by loadEnv is a string. This function can be converted to boolean type
   const viteEnv = wrapperEnv(env);
 
-  // const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
   const isBuild = command === 'build';
   return {
+    base: VITE_PUBLIC_PATH,
     root,
     plugins: createVitePlugins(viteEnv, isBuild),
+    build: {
+      target: 'es2015',
+      outDir: OUTPUT_DIR,
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          // Used to delete console in production environment
+          drop_console: VITE_DROP_CONSOLE
+        }
+      },
+      // Turning off brotliSize display can slightly reduce packaging time
+      brotliSize: false,
+      chunkSizeWarningLimit: 1500
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__)
+    },
     server: {
       host: '0.0.0.0',
-      port: 10000,
-      proxy: {}
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY)
     }
   };
 };
